0271ae016f9411f304a162014251c6f6e9193f4b
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-03-16 00:32:32 -0500

    refactor: ActivityResponse and ClientResponse refactored to implement as record type

--- src/main/java/com/masondubelbeis/clienthubapi/dto/response/ClientResponse.java
+++ src/main/java/com/masondubelbeis/clienthubapi/dto/response/ClientResponse.java
@@ -3,39 +3,10 @@ package com.masondubelbeis.clienthubapi.dto.response;
 import java.time.Instant;
 import java.util.UUID;
 
-public class ClientResponse {
-
-    private UUID id;
-    private String name;
-    private String email;
-    private String phone;
-    private Instant createdAt;
-
-    public ClientResponse(UUID id, String name, String email, String phone, Instant createdAt) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.createdAt = createdAt;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-}
+public record ClientResponse(
+        UUID id,
+        String name,
+        String email,
+        String phone,
+        Instant createdAt
+) {}
