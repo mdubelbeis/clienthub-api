e8b94dbb71e70a2f58ee5b1eada3f09e177c770d
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-03-16 12:41:47 -0500

    feat: add authentication jwt filter before requests

--- src/main/java/com/masondubelbeis/clienthubapi/config/SecurityConfig.java
+++ src/main/java/com/masondubelbeis/clienthubapi/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.masondubelbeis.clienthubapi.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.masondubelbeis.clienthubapi.security.*;
 
+import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,14 +14,35 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
+    private final JwtService jwtService;
+    private final CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(
+            JwtService jwtService,
+            CustomUserDetailsService userDetailsService
+    ) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtService, userDetailsService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
---
+++ src/main/java/com/masondubelbeis/clienthubapi/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.masondubelbeis.clienthubapi.security;
+
+import com.masondubelbeis.clienthubapi.model.User;
+import com.masondubelbeis.clienthubapi.repository.UserRepository;
+
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found")
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
