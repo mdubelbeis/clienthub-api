7ea155c6c15954297b627a712244a18871999468
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-03-16 00:28:23 -0500

    refactor: ErrorResponse refactored to record type

--- src/main/java/com/masondubelbeis/clienthubapi/exception/ErrorResponse.java
+++ src/main/java/com/masondubelbeis/clienthubapi/exception/ErrorResponse.java
@@ -2,45 +2,10 @@ package com.masondubelbeis.clienthubapi.exception;
 
 import java.time.Instant;
 
-public class ErrorResponse {
-
-    private Instant timestamp;
-    private int status;
-    private String error;
-    private String message;
-    private String path;
-
-    public ErrorResponse(
-            Instant timestamp,
-            int status,
-            String error,
-            String message,
-            String path
-    ) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-}
+public record ErrorResponse(
+        Instant timestamp,
+        int status,
+        String error,
+        String message,
+        String path
+){}
