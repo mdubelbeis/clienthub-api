7316d889ad4a0e2da938c67034bffca1f4347a3c
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-03-27 11:32:10 -0500

    Add endpoint for health check

--- src/main/java/com/masondubelbeis/clienthubapi/config/SecurityConfig.java
+++ src/main/java/com/masondubelbeis/clienthubapi/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/actuator/health"
                         ).permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
